f78acd98b067a7c97c8fee17ce81dfbd9b4ac1a8
fadrian06 <franyeradriansanchez@gmail.com>
2025-02-28 14:10:34 -0400

    Extract uninitialized page

--- index.php
+++ index.php
@@ -3,12 +3,9 @@
 use SIPAN\App;
 
 if (!file_exists('vendor/autoload.php')) {
-  exit(<<<html
-    <html data-theme="dark">
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css" />
-      <h1>SE TE OLVIDÓ EJECUTAR <code>composer i</code> :v</h1>
-    </html>
-  html);
+  require_once __DIR__ . '/views/pages/uninitialized.php';
+
+  exit;
 }
 
 require_once __DIR__ . '/vendor/autoload.php';
---
+++ views/pages/uninitialized.php
@@ -0,0 +1,4 @@
+<html data-theme="dark">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css" />
+  <h1>SE TE OLVIDÓ EJECUTAR <code>composer i</code> :v</h1>
+</html>
