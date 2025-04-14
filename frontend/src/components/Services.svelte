3a6f5a2ce524049b67fe88a5f7592009cb3e2cc9
fadrian06 <franyeradriansanchez@gmail.com>
2025-04-14 11:57:47 -0400

    add sveltekit jadoo template

---
+++ frontend/src/components/Services.svelte
@@ -0,0 +1,81 @@
+<script>
+  import shape from '$assets/img/category/shape.svg';
+  import icon1 from '$assets/img/category/icon1.png';
+  import icon2 from '$assets/img/category/icon2.png';
+  import icon3 from '$assets/img/category/icon3.png';
+  import icon4 from '$assets/img/category/icon4.png';
+</script>
+
+<!-- ============================================-->
+<!-- <section> begin ============================-->
+<section class="pt-5 pt-md-9" id="service">
+  <div class="container">
+    <div class="position-absolute z-index--1 end-0 d-none d-lg-block">
+      <img src={shape} style="max-width: 200px" alt="service" />
+    </div>
+    <div class="mb-7 text-center">
+      <h5 class="text-secondary">CATEGORY</h5>
+      <h3 class="fs-xl-10 fs-lg-8 fs-7 fw-bold font-cursive text-capitalize">
+        We Offer Best Services
+      </h3>
+    </div>
+    <div class="row">
+      <div class="col-lg-3 col-sm-6 mb-6">
+        <div
+          class="card service-card shadow-hover rounded-3 text-center align-items-center"
+        >
+          <div class="card-body p-xxl-5 p-4">
+            <img src={icon1} width="75" alt="Service" />
+            <h4 class="mb-3">Calculated Weather</h4>
+            <p class="mb-0 fw-medium">
+              Built Wicket longer admire do barton vanity itself do in it.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-sm-6 mb-6">
+        <div
+          class="card service-card shadow-hover rounded-3 text-center align-items-center"
+        >
+          <div class="card-body p-xxl-5 p-4">
+            <img src={icon2} width="75" alt="Service" />
+            <h4 class="mb-3">Best Flights</h4>
+            <p class="mb-0 fw-medium">
+              Engrossed listening. Park gate sell they west hard for the.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-sm-6 mb-6">
+        <div
+          class="card service-card shadow-hover rounded-3 text-center align-items-center"
+        >
+          <div class="card-body p-xxl-5 p-4">
+            <img src={icon3} width="75" alt="Service" />
+            <h4 class="mb-3">Local Events</h4>
+            <p class="mb-0 fw-medium">
+              Barton vanity itself do in it. Preferd to men it engrossed
+              listening.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-sm-6 mb-6">
+        <div
+          class="card service-card shadow-hover rounded-3 text-center align-items-center"
+        >
+          <div class="card-body p-xxl-5 p-4">
+            <img src={icon4} width="75" alt="Service" />
+            <h4 class="mb-3">Customization</h4>
+            <p class="mb-0 fw-medium">
+              We deliver outsourced aviation services for military customers
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- end of .container-->
+</section>
+<!-- <section> close ============================-->
+<!-- ============================================-->
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+  import Destinations from '$components/Destinations.svelte';
+  import Footer from '$components/Footer.svelte';
+  import Hero from '$components/Hero.svelte';
+  import Navbar from '$components/Navbar.svelte';
+  import Partners from '$components/Partners.svelte';
+  import Services from '$components/Services.svelte';
+  import Steps from '$components/Steps.svelte';
+  import Subscribe from '$components/Subscribe.svelte';
+  import Testimonials from '$components/Testimonials.svelte';
+</script>
+
+<svelte:head>
+  <!-- ===============================================-->
+  <!--    Document Title-->
+  <!-- ===============================================-->
+  <title>Jadoo | Travel Agency Landing Page UI</title>
+</svelte:head>
+
+<!-- ===============================================-->
+<!--    Main Content-->
+<!-- ===============================================-->
+<main class="main" id="top">
+  <Navbar />
+  <Hero />
+  <Services />
+  <Destinations />
+  <Steps />
+  <Testimonials />
+  <Partners />
+  <Subscribe />
+  <Footer />
+</main>
