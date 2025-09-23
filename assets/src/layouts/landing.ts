198102171230e264c340af918857c6914aedc3d9
fadrian06 <franyeradriansanchez@gmail.com>
2025-09-23 19:31:37 -0400

    rename node to $node, I recommend $ for dom references

--- assets/src/layouts/landing.ts
+++ assets/src/layouts/landing.ts
@@ -10,6 +10,6 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 import { Tooltip } from "bootstrap";
 import "alpinejs/dist/cdn.min";
 
-for (const node of document.querySelectorAll('[data-bs-toggle="tooltip"]')) {
-  new Tooltip(node);
+for (const $node of document.querySelectorAll('[data-bs-toggle="tooltip"]')) {
+  new Tooltip($node);
 }
