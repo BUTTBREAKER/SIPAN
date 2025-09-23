d790bfd8bbb04dda9ab51dc330d4741a2b6c965b
fadrian06 <franyeradriansanchez@gmail.com>
2025-09-23 00:03:03 -0400

    extract $products

--- app/Controllers/ProductApiController.php
+++ app/Controllers/ProductApiController.php
@@ -9,6 +9,8 @@ use SIPAN\App;
 final readonly class ProductApiController
 {
   static function index(): void {
-    App::json(db()->select('productos')->all());
+    $products = db()->select('productos')->all();
+
+    App::json($products);
   }
 }
