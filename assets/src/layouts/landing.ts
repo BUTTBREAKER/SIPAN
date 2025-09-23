a3524bf488b674b80dfb449c6e71ef862e3ea4a5
fadrian06 <franyeradriansanchez@gmail.com>
2025-09-22 20:57:07 -0400

    simplify npm packages import

--- assets/src/layouts/landing.ts
+++ assets/src/layouts/landing.ts
@@ -1,7 +1,7 @@
 import "../styles/landing.scss";
 
-import "@fortawesome/fontawesome-free/js/all.min.js";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "@fortawesome/fontawesome-free/js/all.min";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import { Tooltip } from "bootstrap";
 
 for (const node of document.querySelectorAll('[data-bs-toggle="tooltip"]')) {
