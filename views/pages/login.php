1d2393ca7ab6288a4bdc633296cb7b9ebd0655e0
fadrian06 <franyeradriansanchez@gmail.com>
2025-09-23 00:03:47 -0400

    add login form api usage

--- views/pages/login.php
+++ views/pages/login.php
@@ -1,15 +1,31 @@
-<form action="./ingresar" method="post">
-    <div class="mb-3">
-        <label for="correo" class="form-label">Correo Electrónico</label>
-        <input type="email" class="form-control" id="correo" name="correo" required>
-    </div>
-    <div class="mb-3">
-        <label for="clave" class="form-label">Contraseña</label>
-        <input type="password" class="form-control" id="clave" name="clave" required>
-    </div>
-    <button type="submit" class="btn btn-primary w-100">Iniciar Sesión</button>
+<form x-data @submit.prevent="
+  const formData = new FormData($el);
+
+  fetch('./api/ingresar', { body: formData, method: 'post' })
+    .then(async response => {
+      if (!response.ok) {
+        throw new Error(await response.text());
+      }
+
+      return response;
+    })
+    .then(() => {
+      location.href = './administracion';
+    })
+    .catch(error => alert(error.message));
+">
+  <div class="mb-3">
+    <label for="correo" class="form-label">Correo Electrónico</label>
+    <input type="email" class="form-control" id="correo" name="correo" required>
+  </div>
+  <div class="mb-3">
+    <label for="clave" class="form-label">Contraseña</label>
+    <input type="password" class="form-control" id="clave" name="clave"
+      required>
+  </div>
+  <button type="submit" class="btn btn-primary w-100">Iniciar Sesión</button>
 </form>
 
 <p class="text-center mt-3">
-    ¿No tienes cuenta? <a href="./registrarse">Regístrate aquí</a>
+  ¿No tienes cuenta? <a href="./registrarse">Regístrate aquí</a>
 </p>
