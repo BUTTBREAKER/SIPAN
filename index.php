435f975bd6e56c71f444a5401e781fed6dd70ebf
fadrian06 <franyeradriansanchez@gmail.com>
2025-09-22 20:30:48 -0400

    extract $autoloadPath

--- index.php
+++ index.php
@@ -2,13 +2,15 @@
 
 use SIPAN\App;
 
-if (!file_exists('vendor/autoload.php')) {
+$autoloadPath = __DIR__ . '/vendor/autoload.php';
+
+if (!file_exists($autoloadPath)) {
   require_once __DIR__ . '/views/pages/uninitialized.php';
 
   exit;
 }
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once $autoloadPath;
 require_once __DIR__ . '/app/configs.php';
 require_once __DIR__ . '/app/routes.php';
 
