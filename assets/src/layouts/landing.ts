2a800c6e3d0857d2c56e8b9d41a847a3b61ff3b8
fadrian06 <franyeradriansanchez@gmail.com>
2025-09-23 19:28:33 -0400

    move fontawesome import

--- assets/src/layouts/landing.ts
+++ assets/src/layouts/landing.ts
@@ -3,9 +3,9 @@ import "@fontsource/poppins/latin-500.css";
 import "@fontsource/poppins/latin-600.css";
 import "@fontsource/poppins/latin-700.css";
 import "@fontsource/volkhov/latin-700.css";
+import "@fortawesome/fontawesome-free/js/all.min";
 import "../styles/landing.scss";
 
-import "@fortawesome/fontawesome-free/js/all.min";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import { Tooltip } from "bootstrap";
 import "alpinejs/dist/cdn.min";
