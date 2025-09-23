f1bd2eca8e9ef615c64ebdfb8f3c71a52e7f9f10
fadrian06 <franyeradriansanchez@gmail.com>
2025-09-22 20:23:46 -0400

    remove unused $db property in LandingController

--- app/Controllers/LandingController.php
+++ app/Controllers/LandingController.php
@@ -2,13 +2,10 @@
 
 namespace SIPAN\Controllers;
 
-use PDO;
 use SIPAN\App;
 
 final readonly class LandingController
 {
-  function __construct(private PDO $db) {}
-
   function showLanding(): void
   {
     App::renderPage('landing', 'SIPAN', 'landing-layout');
