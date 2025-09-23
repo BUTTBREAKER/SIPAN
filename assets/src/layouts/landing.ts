61e45bd6bd335b66707dd18a238c73a317133cf8
fadrian06 <franyeradriansanchez@gmail.com>
2025-09-22 20:57:32 -0400

    npm i alpinejs@3.15.0 -D

--- assets/src/layouts/landing.ts
+++ assets/src/layouts/landing.ts
@@ -3,6 +3,7 @@ import "../styles/landing.scss";
 import "@fortawesome/fontawesome-free/js/all.min";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import { Tooltip } from "bootstrap";
+import "alpinejs/dist/cdn.min";
 
 for (const node of document.querySelectorAll('[data-bs-toggle="tooltip"]')) {
   new Tooltip(node);
