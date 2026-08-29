ae4260338502d384fb6ade42d6f25538c45a18ca
fadrian06 <franyeradriansanchez@gmail.com>
2026-08-29 18:56:16 -0400

    mejora: agregar verificación de acceso en AuthMiddleware y restaurar rutas de usuarios y sucursales en web.php

--- app/Middlewares/AuthMiddleware.php
+++ app/Middlewares/AuthMiddleware.php
@@ -23,6 +23,10 @@ class AuthMiddleware
                 exit;
             }
 
+            if ($_SERVER['REQUEST_URI'] === '/login') {
+                return;
+            }
+
             header('Location: /login');
             exit;
         }
