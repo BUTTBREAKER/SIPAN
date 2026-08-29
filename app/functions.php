28c1084f1ad1144ecbcdac8870126b1dd8742707
fadrian06 <franyeradriansanchez@gmail.com>
2026-08-29 18:09:10 -0400

    mejora: agregar función getenv para cargar variables de entorno desde .env.example

---
+++ app/functions.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use Symfony\Component\Dotenv\Dotenv;
+
+function getenv(string $name): ?string
+{
+    if (!key_exists($name, $_ENV)) {
+        $dotenv = new Dotenv();
+        $dotenv->load(__DIR__ . '/../.env.example', __DIR__ . '/../.env');
+    }
+
+    return $_ENV[$name] ?? null;
+}
