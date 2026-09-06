e186eda99e5254e2bc4130197743b490bc80e9b7
fadrian06 <franyeradriansanchez@gmail.com>
2026-09-06 19:22:46 -0400

    test(UnitTestCase): inicializar contenedor y factorías PSR en setUp
    Configura el contenedor de dependencias en la clase base de pruebas
    unitarias para automatizar la resolución de interfaces PSR-7.
    
    Se añade una propiedad estática para el contenedor y se registran
    ResponseFactoryInterface y StreamFactoryInterface usando HttpFactory
    dentro del método setUp. Esto evita la duplicación de código en las
    pruebas individuales y asegura que las dependencias de mensajería HTTP
    estén disponibles para los componentes que las requieran.

--- tests/Unit/UnitTestCase.php
+++ tests/Unit/UnitTestCase.php
@@ -4,9 +4,25 @@ declare(strict_types=1);
 
 namespace Tests\Unit;
 
+use flight\Container;
+use GuzzleHttp\Psr7\HttpFactory;
+use Override;
 use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\StreamFactoryInterface;
 
 abstract class UnitTestCase extends TestCase
 {
-    //
+    protected static ContainerInterface $container;
+
+    #[Override]
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $container = Container::getInstance();
+        $container->singleton(ResponseFactoryInterface::class, HttpFactory::class);
+        $container->singleton(StreamFactoryInterface::class, HttpFactory::class);
+        self::$container = $container;
+    }
 }
